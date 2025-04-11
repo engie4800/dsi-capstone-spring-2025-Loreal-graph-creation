651c07a4cd358ecaab515c413e018e87372e955f
JiayiD77 <114614626+JiayiD77@users.noreply.github.com>
2025-04-11 00:19:20 -0400

    Updates

--- Code/Jiayi/create_kg_test.py
+++ Code/Jiayi/create_kg_test.py
@@ -13,11 +13,11 @@ from langchain_community.graphs.graph_document import Node, Relationship
 from dotenv import load_dotenv
 load_dotenv()
 
-DOCS_PATH = "./data/five_samples.csv"
+DOCS_PATH = "./data/cosmetics_25_products.csv"
 
 llm = ChatOpenAI(
     openai_api_key=os.getenv('OPENAI_API_KEY'), 
-    model_name="gpt-3.5-turbo"
+    model_name="gpt-4o-mini"
 )
 
 embedding_provider = OpenAIEmbeddings(
@@ -31,45 +31,106 @@ graph = Neo4jGraph(
     password=os.getenv('NEO4J_PASSWORD')
 )
 
+# doc_transformer = LLMGraphTransformer(
+#     llm=llm,
+#     allowed_nodes=[
+#     "Brand", "Category", "Product", "Price", 
+#     "Rating", "Skin Type", "Size", "Description"
+#     ],
+#     allowed_relationships=[
+#     "FROM_BRAND", "HAS_PRICE", "HAS_RATING", "FOR_SKIN_TYPE",
+#     "IN_SIZE", "HAS_DESCRIPTION", "BELONGS_TO"
+#     ],
+#     node_properties=["productName", "ingredients", "size", "rating", "price",
+#                      "skinType", "description", "brandName", "categoryName"],
+#     strict_mode=True
+#     )
+
 doc_transformer = LLMGraphTransformer(
     llm=llm,
-    allowed_nodes=[
-    "Attribute", "Average rating", "Color", "Company", "Country", "Function", 
-    "Item weight", "Manufacturer", "Material", "Price", "Product Category", "Style"
-    ],
-    allowed_relationships=[
-    "DESIGNED_FOR","DRESS_WITH", "ESSENTIAL_FOR", "FEATURED_MATERIAL", 
-    "FUNCTION", "HAS_AVERAGE_RATING", "HAS_COLOR", "HAS_PRICE", "MANUFACTURED_BY"
-    ]
+    allowed_nodes = ["Product"],
+    node_properties=["productName", "ingredients", "size", "rating", "price",
+                     "forSkinType", "description", "brand", "category"],
+    strict_mode=True
     )
 
 # Load and split the documents
 loader = CSVLoader(file_path=DOCS_PATH)
 
 text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=1500,
-    chunk_overlap=200,
+    separator="\n\n"
 )
 
 docs = loader.load()
 chunks = text_splitter.split_documents(docs)
 
-construction_start_time = time.time()
-for chunk in chunks:
+# construction_start_time = time.time()
+# for chunk in chunks:
 
-    filename = os.path.basename(chunk.metadata["source"])
-    chunk_id = f"{filename}.{chunk.metadata.get('row', '')}"
+#     filename = os.path.basename(chunk.metadata["source"])
+#     chunk_id = f"{filename}.{chunk.metadata.get('row', '')}"
+#     print("Processing -", chunk_id)
+
+#     # Embed the chunk
+#     chunk_embedding = embedding_provider.embed_query(chunk.page_content)
+
+#     # Add the Document and Chunk nodes to the graph
+#     properties = {
+#         "filename": filename,
+#         "chunk_id": chunk_id,
+#         "text": chunk.page_content,
+#         "embedding": chunk_embedding
+#     }
+    
+#     graph.query("""
+#         MERGE (d:Document {id: $filename})
+#         MERGE (c:Chunk {id: $chunk_id})
+#         SET c.text = $text
+#         MERGE (d)<-[:PART_OF]-(c)
+#         WITH c
+#         CALL db.create.setNodeVectorProperty(c, 'textEmbedding', $embedding)
+#         """, 
+#         properties
+#     )
+
+#     # Generate the entities and relationships from the chunk
+#     graph_docs = doc_transformer.convert_to_graph_documents([chunk])
+
+#     # Map the entities in the graph documents to the chunk node
+#     for graph_doc in graph_docs:
+#         chunk_node = Node(
+#             id=chunk_id,
+#             type="Chunk"
+#         )
+
+#         for node in graph_doc.nodes:
+#             if node.type == "Product":
+#                 graph_doc.relationships.append(
+#                     Relationship(
+#                         source=chunk_node,
+#                         target=node, 
+#                         type="HAS_PRODUCT"
+#                     )
+#                 )
+
+#     # add the graph documents to the graph
+#     graph.add_graph_documents(graph_docs)
+# construction_end_time = time.time()
+construction_start_time = time.time()
+for doc in docs:
+
+    filename = os.path.basename(doc.metadata["source"])
+    chunk_id = f"{filename}.{doc.metadata.get('row', '')}"
     print("Processing -", chunk_id)
 
     # Embed the chunk
-    chunk_embedding = embedding_provider.embed_query(chunk.page_content)
+    chunk_embedding = embedding_provider.embed_query(doc.page_content)
 
     # Add the Document and Chunk nodes to the graph
     properties = {
         "filename": filename,
         "chunk_id": chunk_id,
-        "text": chunk.page_content,
+        "text": doc.page_content,
         "embedding": chunk_embedding
     }
     
@@ -85,7 +146,7 @@ for chunk in chunks:
     )
 
     # Generate the entities and relationships from the chunk
-    graph_docs = doc_transformer.convert_to_graph_documents([chunk])
+    graph_docs = doc_transformer.convert_to_graph_documents([doc])
 
     # Map the entities in the graph documents to the chunk node
     for graph_doc in graph_docs:
@@ -95,12 +156,12 @@ for chunk in chunks:
         )
 
         for node in graph_doc.nodes:
-
-            graph_doc.relationships.append(
-                Relationship(
-                    source=chunk_node,
-                    target=node, 
-                    type="HAS_ENTITY"
+            if node.type == "Product":
+                graph_doc.relationships.append(
+                    Relationship(
+                        source=chunk_node,
+                        target=node, 
+                        type="HAS_PRODUCT"
                     )
                 )
 
